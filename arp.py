00144fdf3a79f24a93b1b48c68b46a44a4ed7441
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-04-29 22:01:21 -0400

    Adjusting vectorizer. Adding timers. Formatting ouputs. Adding ROC curve.

--- arp.py
+++ arp.py
@@ -1,11 +1,14 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scikitplot as skplt
+from sklearn.feature_extraction import stop_words
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, recall_score, average_precision_score,precision_recall_curve, roc_curve, auc
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction import stop_words
 from sklearn.preprocessing import StandardScaler
+from time import time
 
 
 # Datatypes of each column
@@ -43,14 +46,13 @@ print('Not Recommended: %s' % data.where(data['recommended'] == 0).dropna(how='a
 print('Recommended: %s' % data.where(data['recommended'] == 1).dropna(how='all').shape[0])
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(data['content'],
-                                                    data['recommended'],
-                                                    random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(data['content'], data['recommended'], random_state=1)
 
 # Vectorize
 # N-gram and Bigram
 ngram_size = 2
-vectorizer = TfidfVectorizer(min_df=1,
+tic = time()
+vectorizer = TfidfVectorizer(min_df=2,  # Remove words only appearing in a single document
                              ngram_range=(ngram_size, ngram_size),
                              # the analyzer determines how you split up your ngram, i think 'word' is default?
                              # 'char' would be the character ngrams you were talking about
@@ -61,18 +63,37 @@ train_ngram = vectorizer.transform(X_train)
 # scaling the data achieves worse results, but maybe its the ethical thing to do in data science?
 # train_ngram = StandardScaler(with_mean=False).fit_transform(train_ngram)
 test_ngram = vectorizer.transform(X_test)
+toc = time()
+print('Vectorization Time: %.02fs' % (toc - tic))
+
 # Naive Bayes
+tic = time()
 naive_bayes = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
 naive_bayes.fit(train_ngram, y_train)
+toc = time()
+print('Training Time: %0.2fs' % (toc - tic))
 
 # predict the validation set
+tic = time()
 predictions = naive_bayes.predict(test_ngram)
+toc = time()
+print('Prediction Time: %0.2fs' % (toc - tic))
+
+predicted_probas = naive_bayes.predict_proba(test_ngram)
 
 
 # Validate
-print('Accuracy: ' + format(accuracy_score(y_test, predictions)))
-print('Precision: ' + format(precision_score(y_test, predictions)))
-print('Recall: ' + format(recall_score(y_test, predictions)))
-print('Confusion:\n' + format(confusion_matrix(y_test, predictions)))
+print('Accuracy: %0.3f' % accuracy_score(y_test, predictions))
+print('Precision: %0.3f' % precision_score(y_test, predictions))
+print('Recall: %0.3f' % recall_score(y_test, predictions))
+print('Confusion:')
+conmat = np.array(confusion_matrix(y_test, predictions, labels=[1, 0]))
+confusion = pd.DataFrame(conmat, index=['positive', 'negative'],
+                         columns=['predicted_positive', 'predicted_negative'])
+print(confusion)
+
+# ROC Curve
+skplt.metrics.plot_roc_curve(y_test, predicted_probas)
+plt.show()
 
 # Done
